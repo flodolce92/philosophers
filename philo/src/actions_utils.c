354475d4420a8f9e9de9ce001abc732c38301ca3
Federico Lo Dolce <flodolce92@gmail.com>
2024-12-22 22:17:19 +0100

    norm fix

--- philo/src/actions_utils.c
+++ philo/src/actions_utils.c
@@ -6,7 +6,7 @@
 /*   By: flo-dolc <flo-dolc@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 14:55:58 by flo-dolc          #+#    #+#             */
-/*   Updated: 2024/12/22 19:23:21 by flo-dolc         ###   ########.fr       */
+/*   Updated: 2024/12/22 22:14:42 by flo-dolc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,37 +19,28 @@ void	message(char *msg, t_philo *philo)
 	pthread_mutex_unlock(&philo->table->print_lock);
 }
 
-int sleep_till_death(size_t time, t_philo *philo)
+int	sleep_till_death(size_t time, t_philo *philo)
 {
-	size_t start;
+	size_t	start;
 
 	start = now();
 	if (start + time > philo->eta_death)
 	{
-		// message("sleeping till death\n", philo);
-		// printf("time: %lu\n", time);
-		// printf("start: %lu eta_death: %lu\n", start, philo->eta_death);
 		time = philo->eta_death - start;
 		if (philo->eta_death < start)
-		{
 			time = 0;
-			// printf("RESET TIME\n");
-		}
-		// printf("time to sleep: %lu\n", time);
-		// message("DEATH BY DEATH\n", philo);
 		usleep(time * 1000);
 		pthread_mutex_lock(&philo->table->dead_lock);
 		p_death(philo);
 		pthread_mutex_unlock(&philo->table->dead_lock);
-		return (message("DEATH BY TIME\n", philo) , 1);
+		return (message("DEATH BY TIME\n", philo), 1);
 	}
 	usleep(time * 1000);
 	return (0);
 }
 
-int check_death(t_philo *philo)
+int	check_death(t_philo *philo)
 {
-	// printf("check_death philo n. %d\n", philo->id);
 	if (philo->table->death_flag == 1)
 	{
 		pthread_mutex_unlock(&philo->table->dead_lock);
@@ -64,15 +55,11 @@ int check_death(t_philo *philo)
 	return (0);
 }
 
-int take_fork(t_fork *fork, t_philo *philo)
+int	take_fork(t_fork *fork, t_philo *philo)
 {
 	while (1)
 	{
-		// message("trying to take a fork\n", philo);
-		// printf("philo %d trying to lock mux\n", philo->id);
 		pthread_mutex_lock(&fork->fork_lock);
-		// message("fork locked\n", philo);
-		// printf("philo %d trying to take fork %d\n", philo->id, fork->in_use);
 		if (fork->in_use == 0)
 		{
 			fork->in_use = 1;
@@ -80,7 +67,6 @@ int take_fork(t_fork *fork, t_philo *philo)
 			break ;
 		}
 		pthread_mutex_unlock(&fork->fork_lock);
-		// message("fork unlocked\n", philo);
 		if (sleep_till_death(1, philo))
 			return (message("DEATH BY FORK ABSENCE\n", philo), 1);
 	}
@@ -88,14 +74,11 @@ int take_fork(t_fork *fork, t_philo *philo)
 	if (check_death(philo))
 		return (1);
 	pthread_mutex_unlock(&philo->table->dead_lock);
-	// pthread_mutex_lock(&philo->table->print_lock);
-	// printf("%lu %d has taken a fork\n", now(), philo->id);
 	message("has taken a fork\n", philo);
-	// pthread_mutex_unlock(&philo->table->print_lock);
 	return (0);
 }
 
-void drop_forks(t_philo *philo)
+void	drop_forks(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->right_fork->fork_lock);
 	philo->right_fork->in_use = 0;
--- philo/src/threads.c
+++ philo/src/threads.c
@@ -6,7 +6,7 @@
 /*   By: flo-dolc <flo-dolc@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 16:40:38 by flo-dolc          #+#    #+#             */
-/*   Updated: 2024/12/22 18:38:43 by flo-dolc         ###   ########.fr       */
+/*   Updated: 2024/12/22 22:16:01 by flo-dolc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	*routine_philo(void *p)
 {
 	t_philo	*philo;
-	int 	eaten_meals;
+	int		eaten_meals;
 
 	philo = (t_philo *) p;
 	eaten_meals = 0;
@@ -28,11 +28,9 @@ void	*routine_philo(void *p)
 	while (eaten_meals != philo->table->max_meals)
 	{
 		pthread_mutex_lock(&philo->table->dead_lock);
-		// message("locked dead_lock\n", philo);
 		if (check_death(philo))
 			return (NULL);
 		pthread_mutex_unlock(&philo->table->dead_lock);
-		// message("unlocked dead_lock\n", philo);
 		if (p_eat(philo) || p_sleep(philo) || p_think(philo))
 			return (NULL);
 		if (philo->table->max_meals != -1)
@@ -48,7 +46,8 @@ void	start_dinner(t_table *table)
 	i = 0;
 	while (i < table->n_philo)
 	{
-		if (pthread_create(&table->philos[i].thread, NULL, routine_philo, &table->philos[i]))
+		if (pthread_create(&table->philos[i].thread, NULL, \
+		routine_philo, &table->philos[i]))
 			throw_error("pthread_create failed", table, 1);
 		i++;
 	}
